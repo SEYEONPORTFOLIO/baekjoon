403ec7d09d4f8c316059ce99ae37dacd45b6a1be
LEE SE YEON <121601875+SEYEONPORTPOLIO@users.noreply.github.com>
2023-01-12 23:15:03 +0900

    Add files via upload

---
+++ 조건문/C++/알람시계.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int h, m;
+	cin >> h >> m;
+	if (m - 45 < 0) {
+		m = 60 + (m - 45);
+		if (h == 0) h = 23;
+		else --h;
+	}
+	else {
+		m -= 45;
+	}
+	cout << h <<" "<< m;
+	return 0;
+}

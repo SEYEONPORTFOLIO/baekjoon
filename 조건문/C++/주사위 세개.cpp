1db0ff02526f1950de4b105c23757cde450cf474
LEE SE YEON <121601875+SEYEONPORTPOLIO@users.noreply.github.com>
2023-01-20 23:29:05 +0900

    Add files via upload

---
+++ 조건문/C++/주사위 세개.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b, c;
+	int money;
+	cin >> a >> b >> c;
+	if ((a == b) && (b == c) && (a == c)) money = 10000 + a * 1000;
+	else if ((a == b) && (b != c)) money = 1000 + a * 100;
+	else if ((b == c) && (a != c)) money = 1000 + b * 100;
+	else if ((a == c) && (b != c)) money = 1000 + c * 100;
+	else if ((a != b) && (b != c) && (a != c)) {
+		int max = a;
+		if (a < b) {
+			(b < c) ? max = c : max = b;
+		}
+		else if (a < c) {
+			max = c;
+		}
+		money = max * 100;
+	}
+	cout << money;
+	return 0;
+}
